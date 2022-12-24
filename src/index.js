148eda663b3878209a869b1e3480dd40ecf0c700
JhonSpr <jhoreyes20h@gmail.com>
2022-12-24 13:29:23 -0500

    a

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
 const { swaggerDocs: V1SwaggerDocs } = require("./v1/swagger");
 
 const app = express();
-const PORT = process.env.PORT || 0;
+const PORT = process.env.PORT || 80;
 const cache = apicache.middleware;
 
 app.use(express.json());
 app.use(cache("2 minutes"));
-app.use("/api/v1/workouts/", v1WorkoutRouter);
+app.use("https://animekuns.vercel.app/api/v1/workouts/", v1WorkoutRouter);
 
 app.listen(PORT, () => {
   console.log(`🚀 Server listening on port ${PORT}`);
