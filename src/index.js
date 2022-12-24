d9571407c4c8b72257954f425c67bca499460cb9
JhonSpr <jhoreyes20h@gmail.com>
2022-12-24 16:57:18 -0500

    Aa

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
 const { swaggerDocs: V1SwaggerDocs } = require("./v1/swagger");
 
 const app = express();
-const PORT = process.env.API_URL;
+const API_URL = process.env.API_URL;
 const cache = apicache.middleware;
 
 app.use(express.json());
