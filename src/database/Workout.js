54ee8d6fb308359956360c110ca893057925be2d
JhonSpr <jhoreyes20h@gmail.com>
2023-01-09 17:02:14 -0500

    a

--- src/database/Workout.js
+++ src/database/Workout.js
@@ -53,9 +53,10 @@ const getAllWorkouts = (filterParams) => {
       );
     }
     if (filterParams.year) {
-      return DB.workouts.filter(
-        (workout) => workout.year === 2018 || workout.year === 2019
-      );
+      return DB.workouts.filter((workout) => workout.year === 2018);
+    }
+    if (filterParams.year) {
+      return DB.workouts.filter((workout) => workout.year === 2019);
     }
 
     return workouts;
