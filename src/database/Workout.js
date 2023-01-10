da04cec1f5691c63d4f8163e1f974d1cf857cd58
JhonSpr <jhoreyes20h@gmail.com>
2023-01-09 23:25:44 -0500

    a

--- src/database/Workout.js
+++ src/database/Workout.js
@@ -60,7 +60,7 @@ const getAllWorkouts = (filterParams) => {
     }
     if (filterParams.estado) {
       return DB.workouts.filter(
-        (anime) => anime.estado === "Finalizado" || "En emision"
+        (anime) => anime.estado === "Finalizado" || "En Emision"
       );
     }
 
