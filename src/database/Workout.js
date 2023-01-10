e144c2d626038d7bdf7469c1efbff1113b76f126
JhonSpr <jhoreyes20h@gmail.com>
2023-01-09 23:20:57 -0500

    a

--- src/database/Workout.js
+++ src/database/Workout.js
@@ -60,7 +60,7 @@ const getAllWorkouts = (filterParams) => {
     }
     if (filterParams.estado) {
       return DB.workouts.filter(
-        (anime) => anime.estado === anime.estado.includes(filterParams.estado)
+        (anime) => anime.estado === "en emision".includes(filterParams.estado)
       );
     }
 
