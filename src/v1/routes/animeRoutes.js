667b097f818bde457062e025bb9ccd769b9d3acb
JhonSpr <jhoreyes20h@gmail.com>
2023-02-07 01:12:29 -0500

    a

--- src/v1/routes/animeRoutes.js
+++ src/v1/routes/animeRoutes.js
@@ -8,35 +8,7 @@ router
   .get("/:workoutId", animeController.getOneWorkout)
   .post("/", animeController.createNewWorkout)
   .patch("/:workoutId", animeController.updateOneWorkout)
-  .put("/:id", (req, res) => {
-    fs.readFile("../../database/db.json", "utf-8", (err, data) => {
-      if (err) {
-        return res.status(500).send({error: "Error reading products file"});
-      }
 
-      const products = JSON.parse(data);
-      const product = products.find((p) => p.id === req.params.id);
-
-      if (!product) {
-        return res.status(404).send({error: "Product not found"});
-      }
-
-      product.name = req.body.name;
-      product.price = req.body.price;
-
-      fs.writeFile(
-        "../../database/db.json",
-        JSON.stringify(products),
-        (writeErr) => {
-          if (writeErr) {
-            return res.status(500).send({error: "Error writing products file"});
-          }
-
-          return res.send({message: "Product updated successfully"});
-        },
-      );
-    });
-  })
   .delete("/:workoutId", animeController.deleteOneWorkout);
 
 module.exports = router;
