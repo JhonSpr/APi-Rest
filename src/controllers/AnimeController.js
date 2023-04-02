3e9d21866ac36691e4c10705a62453b3040440b2
JhonSpr <jhonreyes20h@gmail.com>
2023-04-02 17:40:45 -0500

    a

--- src/controllers/AnimeController.js
+++ src/controllers/AnimeController.js
@@ -19,11 +19,6 @@ const getAllAnimes = (req, res) => {
       genero,
       tipo,
     });
-    const sortedData = animes.sort((a, b) => {
-      if (a.name < b.name) return -1;
-      if (a.name > b.name) return 1;
-      return 0;
-    });
 
     const paginatedData = animes.slice(startIndex, endIndex);
     const datos = animes.slice(startIndex, endIndex);
