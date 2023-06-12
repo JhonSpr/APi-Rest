454795e534b611e679260bb3c5acca3080fd6737
Jhonspr <jhonreyes20h@gmail.com>
2023-06-12 15:42:03 -0500

    a

--- src/controllers/AnimeController.js
+++ src/controllers/AnimeController.js
@@ -23,8 +23,7 @@ const getAllAnimes = (req, res) => {
 
     let datos = animes;
     if (orderBy == "asc" || orderBy == "desc") {
-      return;
-      datos.sort((a, b) => {
+      datos = datos.sort((a, b) => {
         if (orderBy === "asc") {
           return a.name.localeCompare(b.name);
         } else if (orderBy === "desc") {
