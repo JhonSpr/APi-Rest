bc4230addc206d745a990ff1e2bfc54717655018
Jhonspr <jhonreyes20h@gmail.com>
2023-07-24 19:51:18 -0500

    a

--- src/index.js
+++ src/index.js
@@ -1,20 +1,16 @@
 const express = require("express");
-const apicache = require("apicache");
 const cors = require("cors");
 const v1animeRouter = require("./v1/routes/animeRoutes");
 const db = require("./database/Recientes.json");
-const { swaggerDocs: V1SwaggerDocs } = require("./v1/swagger");
 const app = express();
 const PORT = process.env.PORT || 3001;
-const cache = apicache.middleware;
 app.use(cors());
 app.use(express.json());
-app.use(cache("2 minutes"));
 app.use("/api/v1/animes/", v1animeRouter);
-app.use("/api/v1/recien-agregados", (re, res) => {
+app.use("/api/v1/recien-agregados", (req, res) => {
   res.send({ recientes: db.recientes });
 });
 app.listen(PORT, () => {
   console.log(`🚀 Server listening on port ${PORT}`);
-  V1SwaggerDocs(app, PORT);
+  app, PORT;
 });
