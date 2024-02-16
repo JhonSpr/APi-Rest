fe774efd74479baf8fcec88856c6227439e87ce9
Jhonspr <jhonreyes20hgmail.com>
2024-02-15 19:48:09 -0500

    .

--- src/controllers/AnimeController.js
+++ src/controllers/AnimeController.js
@@ -1,18 +1,19 @@
-const animeService = require("../services/AnimeService");
+const animeService = require('../services/AnimeService')
 
 const getAllAnimes = (req, res) => {
-  const { name, estado, info } = req.query;
+  const { name, estado, info } = req.query
   try {
-    const limit = parseInt(req.query.limit) || 24;
-    const page = parseInt(req.query.page) || 1;
-    const startIndex = (page - 1) * limit;
-    const endIndex = startIndex + limit;
-    const años = req.query.años;
-    const episodes = req.query.episodes;
-    const genero = req.query.genero;
-    const type = req.query.type;
-    const rating = req.query.rating;
-    const sortBy = req.query.sortBy ?? "todos";
+    const limit = parseInt(req.query.limit) || 24
+    const page = parseInt(req.query.page) || 1
+    const startIndex = (page - 1) * limit
+    const endIndex = startIndex + limit
+    const años = req.query.años
+    const episodes = req.query.episodes
+    const genero = req.query.genero
+    const type = req.query.type
+    const rating = req.query.rating
+    const sortBy = req.query.sortBy ?? 'todos'
+    const letra = req.query.letra
     const animes = animeService.getAllAnime({
       name,
       años,
@@ -23,23 +24,24 @@ const getAllAnimes = (req, res) => {
       info,
       rating,
       sortBy,
-    });
-    let datos = animes;
+      letra,
+    })
+    let datos = animes
 
     res.send({
       datos: datos.slice(startIndex, endIndex),
       item: datos.length,
       currentPage: page,
       sort: sortBy,
-    });
+    })
   } catch (error) {
     res.status(error?.status || 500).send({
-      status: "Algo salió mal",
+      status: 'Algo salió mal',
       data: [],
-    });
+    })
   }
-};
+}
 
 module.exports = {
   getAllAnimes,
-};
+}
--- src/database/AnimeModel.js
+++ src/database/AnimeModel.js
@@ -1,63 +1,71 @@
-const DB = require("./db.json");
-const originalData = [...DB.animes];
+const DB = require('./db.json')
+const originalData = [...DB.animes]
 
 const getAllAnime = (filterParams) => {
   try {
-    const minRating = parseFloat(filterParams.rating);
-    const maxRating = minRating + 0.9;
+    const minRating = parseFloat(filterParams.rating)
+    const maxRating = minRating + 0.9
 
     // Restaurar los datos originales cada vez que se llama a la función
-    let animes = [...originalData];
+    let animes = [...originalData]
 
-    if (filterParams.info) {
-      return animes.filter((anime) => anime.name == filterParams.info);
+    if (filterParams.letra) {
+      return animes.filter(
+        (anime) =>
+          anime.name.charAt(0).toLowerCase() ===
+          filterParams.letra.toLowerCase()
+      )
     }
 
-    if (filterParams.name !== undefined && filterParams.name.trim() !== "") {
+    if (filterParams.info) {
+      return animes.filter((anime) => anime.name == filterParams.info)
+    }
+
+    if (filterParams.name !== undefined && filterParams.name.trim() !== '') {
       animes = animes.filter((anime) =>
-        anime.name.toLowerCase().includes(filterParams.name.toLowerCase()),
-      );
+        anime.name.toLowerCase().includes(filterParams.name.toLowerCase())
+      )
     }
 
     if (filterParams.años) {
       const años = Array.isArray(filterParams.años)
         ? filterParams.años
-        : [filterParams.años];
+        : [filterParams.años]
 
       animes = animes.filter((animeItem) =>
-        años.some((año) => animeItem.year == año),
-      );
+        años.some((año) => animeItem.year == año)
+      )
     }
 
     if (filterParams.estado) {
       const estado = Array.isArray(filterParams.estado)
         ? filterParams.estado
-        : [filterParams.estado];
+        : [filterParams.estado]
       animes = animes.filter((animeItem) =>
-        estado.some((estado) => animeItem.estado == estado),
-      );
+        estado.some((estado) => animeItem.estado == estado)
+      )
     }
 
     if (filterParams.type) {
       const type = Array.isArray(filterParams.type)
         ? filterParams.type
-        : [filterParams.type];
+        : [filterParams.type]
 
       animes = animes.filter((animeItem) =>
-        type.some((type) => animeItem.tipo === type),
-      );
+        type.some((type) => animeItem.tipo === type)
+      )
     }
 
     if (filterParams.episodes) {
       animes = animes.filter(
-        (anime) => anime.episodios === String(filterParams.episodes),
-      );
+        (anime) => anime.episodios === String(filterParams.episodes)
+      )
     }
 
     if (filterParams.genero) {
       const generos = Array.isArray(filterParams.genero)
         ? filterParams.genero
-        : [filterParams.genero];
+        : [filterParams.genero]
 
       animes = animes.filter((anime) =>
         generos.some((genero) =>
@@ -69,32 +77,32 @@ const getAllAnime = (filterParams) => {
             anime.genero5,
             anime.genero6,
             anime.genero7,
-          ].includes(genero.toLowerCase()),
-        ),
-      );
+          ].includes(genero.toLowerCase())
+        )
+      )
     }
 
-    if (filterParams.sortBy === "desc") {
-      animes.sort((a, b) => b.name.localeCompare(a.name));
-    } else if (filterParams.sortBy === "asc") {
-      animes.sort((a, b) => a.name.localeCompare(b.name));
+    if (filterParams.sortBy === 'desc') {
+      animes.sort((a, b) => b.name.localeCompare(a.name))
+    } else if (filterParams.sortBy === 'asc') {
+      animes.sort((a, b) => a.name.localeCompare(b.name))
     }
     if (filterParams.rating) {
-      const ratingQueryParam = filterParams.rating.toLowerCase();
+      const ratingQueryParam = filterParams.rating.toLowerCase()
 
-      if (ratingQueryParam === "mayor") {
-        animes = animes.filter((animeItem) => animeItem.rating > 6);
-      } else if (ratingQueryParam === "menor") {
-        animes = animes.filter((animeItem) => animeItem.rating < 6);
+      if (ratingQueryParam === 'mayor') {
+        animes = animes.filter((animeItem) => animeItem.rating > 6)
+      } else if (ratingQueryParam === 'menor') {
+        animes = animes.filter((animeItem) => animeItem.rating < 6)
       }
     }
 
-    return animes;
+    return animes
   } catch (error) {
-    throw { status: 500, message: error };
+    throw { status: 500, message: error }
   }
-};
+}
 
 module.exports = {
   getAllAnime,
-};
+}
